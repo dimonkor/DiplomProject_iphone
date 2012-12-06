511aefdd69b858629e0e699570ace59486294564
dimonkor <dimon_kor@bk.ru>
2012-12-06 23:14:51 +0400

    Initial Commit

---
+++ DiplomProject/DPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DPAppDelegate.h
+//  DiplomProject
+//
+//  Created by Dmitry Korotchenkov on 06.12.12.
+//  Copyright (c) 2012 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiplomProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DiplomProject
+//
+//  Created by Dmitry Korotchenkov on 06.12.12.
+//  Copyright (c) 2012 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAppDelegate class]));
+    }
+}
